0b13e458f6ac8e3b7d57498670cb306aeda11bc1
ProgramerPritom <badruzzamanpritom@gmail.com>
2022-04-27 02:31:16 +0600

    added footer section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import NotFound from './NotFoundPage/NotFound';
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login/Login';
+import ServiceDetails from './Pages/ServiceDetails/ServiceDetails';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
 
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/service/:serviceId' element={<ServiceDetails></ServiceDetails>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,22 +1,28 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome } from '@fortawesome/free-solid-svg-icons';
-// import {  facebook} from '@fortawesome/free-brands-svg-icons'
+import './Footer.css';
+
 
 const Footer = () => {
     return (
         <div>
-            <div className="container-fluid">
+            <div className="container-fluid footer-area text-white">
                 <div className="row">
-                    <div className="col-md-4">
+                    <div className="col-md-4 text-center mt-3">
                         <h4>Moment Studio Agency</h4>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel debitis unde, recusandae aut tempore amet.</p>
-                        <p><span><font-awesome-icon icon={faHome} /></span></p>
+                        <p><span>Copyrights by Moment Agency &copy; 2022</span></p>
 
                     </div>
                     <div className="col-md-4"></div>
-                    <div className="col-md-2"></div>
-                    <div className="col-md-2"></div>
+                    <div className="col-md-2 mt-2">
+                        <p>About</p>
+                        <p>Blogs</p>
+                        <p>Services</p>
+                        <p>Contact us</p>
+                    </div>
+                    <div className="col-md-2">
+                        
+                    </div>
                 </div>
             </div>
         </div>
