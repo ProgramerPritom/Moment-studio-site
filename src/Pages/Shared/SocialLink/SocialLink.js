fec5d27b2e8114293d5b672168559d401ee00f3b
ProgramerPritom <badruzzamanpritom@gmail.com>
2022-04-30 15:58:03 +0600

    signIn with google and github set done

--- src/Pages/Shared/SocialLink/SocialLink.js
+++ src/Pages/Shared/SocialLink/SocialLink.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import './SocialLink.css';
 import googleLogo from '../../../images/Social/google.png';
-import facebookLogo from '../../../images/Social/facebook.png';
+
 import githubLogo from '../../../images/Social/github.png';
+import { useSignInWithGithub, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
+import { useLocation, useNavigate } from 'react-router-dom';
+import Loading from '../../Login/Loading/Loading';
 
 const SocialLink = () => {
+    const navigate = useNavigate();
+    const location = useLocation();
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const [signInWithGithub, user1, loading1, error1] = useSignInWithGithub(auth);
+
+    let from = location.state?.from?.pathname || "/";
+    if (user || user1) {
+        navigate(from, { replace: true })
+    }
+    if(loading || loading1){
+        return <Loading></Loading>
+    }
+    let ErrorMessage;
+    if (error || error1) {
+        ErrorMessage = <div>
+        <p className='text-danger'>Error: {error?.message} {error1?.message}</p>
+    </div>
+    }
+
+   
+
     return (
         <div>
             <div className="social-link-area d-flex align-items-center">
@@ -12,14 +37,13 @@ const SocialLink = () => {
                 <div className='mt-2 px-2'>Try one</div>
                 <div className='line-secend w-50 bg-primary'></div>
             </div>
+            {ErrorMessage}
             <div className="button-area">
-                <button className='btn btn-info w-50 d-block mx-auto mb-2 mt-2'>
+                <button onClick={() => signInWithGoogle()} className='btn btn-info w-50 d-block mx-auto mb-2 mt-2'>
                     <img style={{width:"25px"}} className='mx-2' src={googleLogo} alt="" />
                     Google</button>
-                <button className='btn btn-info w-50 d-block mx-auto mb-2'>
-                    <img style={{width:"25px"}} className='mx-2' src={facebookLogo} alt="" />
-                    Facebook</button>
-                <button className='btn btn-info w-50 d-block mx-auto mb-2'>
+                
+                <button onClick={() => signInWithGithub()} className='btn btn-info w-50 d-block mx-auto mb-2'>
                     <img style={{width:"25px"}} className='mx-2' src={githubLogo} alt="" />
                     Github</button>
             </div>
